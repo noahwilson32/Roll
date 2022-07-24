187cd9a6d1a2d9a69b11ad8a7debbf22d6e7decd
Noah Wilson <noahwilsonmailbox32@gmail.com>
2022-07-23 23:04:03 -0400

    next commit

---
+++ DiceRoll/Assets/Scripts/DragController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private bool isClicking = false;
+
+    [SerializeField]
+    Vector2 mousePos;
+
+    [SerializeField]
+    private Vector3 worldCords;
+
+    public Camera mainCamera;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isClicking = true;
+            mousePos = Input.mousePosition;
+            Debug.Log(mousePos);
+            worldCords = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else 
+        {
+            isClicking = false;
+        }
+    }
+}
